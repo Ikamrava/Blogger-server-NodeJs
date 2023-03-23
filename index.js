ef7d9181cfb780100d819ae314ef4f7bfac4e239
ikamrava <kamrava.iman@outlook.com>
2023-03-23 20:29:38 +0000

    make it better

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import cors from "cors"
 import mysql from 'mysql'
 
 export const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password : process.env.MYSQL_PASSWORD,
-    database : process.env.MYSQL_DATABASE
+    host: "sql8.freesqldatabase.com",
+    port: "3306",
+    user: "sql8606846",
+    password : "hRY3mspJEF",
+    database : "sql8606846"
 })
 
 
