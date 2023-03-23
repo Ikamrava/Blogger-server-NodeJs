d44779eecbd1d6d3bb67c4baa32978a384b911d4
ikamrava <kamrava.iman@outlook.com>
2023-03-23 19:27:40 +0000

    make it better

--- index.js
+++ index.js
@@ -17,15 +17,22 @@ app.listen(process.env.PORT || "8020",()=>{
 
 
 app.post("/register",(req,res)=>{
-   console.log(req.body)
+  console.log(req.body)
+  const q = "select * from users where email = ? or username = ?"
+    db.query(q,[req.body.email,req.body.username],(err,data)=>{
+        if(err) return res.json(err)
+        if (data.length) return res.status(409).json("User already exist")
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(req.body.password, salt)
+
+        const qu = "insert into users(`username,email,password`) values (?)" 
+        const values = [req.body.username,req.body.email,hash]
+        db.query(qu,[values],(err,data)=>{
+            if (err) return res.json(err)
+            // return res.status(200).json("User has been created")
+        })
     })
-  // connection.connect((error)=>{
-  //   if(error){
-  //    console.log(error)
-  //   }else {
-      
-  //   }
-  // })
+    })
   
     
 
