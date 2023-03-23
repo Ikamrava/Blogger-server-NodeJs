3c65ebccaa2a51927fe46e428d19f0352744dc21
ikamrava <kamrava.iman@outlook.com>
2023-03-23 20:00:56 +0000

    make it better

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import {db} from "./db.js"
 import express from 'express'
 import * as dotenv from 'dotenv' 
+import cors from "cors"
+
 
 
 
 const app = express()
 app.use(express.json())
 dotenv.config()
+app.use(cors())
 
 
 
@@ -19,19 +22,21 @@ app.listen(process.env.PORT || "8020",()=>{
 app.post("/register",(req,res)=>{
   console.log(req.body)
   const q = "select * from users where email = ? or username = ?"
-    db.query(q,[req.body.email,req.body.username],(err,data)=>{
-        if(err) return res.json(err)
-        if (data.length) return res.status(409).json("User already exist")
-        const salt = bcrypt.genSaltSync(10)
-        const hash = bcrypt.hashSync(req.body.password, salt)
+    // db.query(q,[req.body.email,req.body.username],(err,data)=>{
+    //     if(err) return res.json(err)
+    //     if (data.length) return res.status(409).json("User already exist")
+  
+        // const salt = bcrypt.genSaltSync(10)
+        // const hash = bcrypt.hashSync(req.body.password, salt)
 
-        const qu = "insert into users(`username,email,password`) values (?)" 
-        const values = [req.body.username,req.body.email,hash]
+        const qu = "INSERT INTO users(username,email,password) Values (?,?,?)" 
+        const values = [req.body.username,req.body.email,req.body.password]
         db.query(qu,[values],(err,data)=>{
-            if (err) return res.json(err)
+            if (err) console.log(err)
+            console.log(data)
             // return res.status(200).json("User has been created")
         })
-    })
+    
     })
   
     
