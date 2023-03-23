66c74c4ccdbe7738edbd74c660c45832b5b46c90
ikamrava <kamrava.iman@outlook.com>
2023-03-23 20:21:41 +0000

    make it better

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import {db} from "./db.js"
 import express from 'express'
 import * as dotenv from 'dotenv' 
 import cors from "cors"
+import mysql from 'mysql'
+
+export const db = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password : process.env.MYSQL_PASSWORD,
+    database : process.env.MYSQL_DATABASE
+})
 
 
 
